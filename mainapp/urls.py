a0a3bf8c77326a786d4ba350471b7a5780c5549a
DHEERAJ KUMAR <dheerajrao20@outlook.com>
2022-04-30 19:40:04 +0530

    importing image in python files

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,4 +3,6 @@ from mainapp import views
 
 urlpatterns = [ 
     path('', views.home, name='home'),
+    path('upload', views.upload, name='upload'),
+    path('download', views.download, name='download'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+
+from django.shortcuts import redirect, render
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
 def home(request):
     return render(request, 'index.html')
+
+def upload(request):
+    if request.method == 'POST':
+        image1 = request.POST['file1']
+        return image1
+
+    return render(request, 'upload.html')
+
+def download(request, image1):
+    return render(request ,'download.html', {'image1':image1})
