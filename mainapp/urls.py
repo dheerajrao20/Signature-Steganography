53b58b7b633b806dd20fbf94c25a437c1a8dd35f
DHEERAJ KUMAR <dheerajrao20@outlook.com>
2022-05-29 10:23:58 +0530

    adding new files of backend

--- mainapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from mainapp import views
-
-urlpatterns = [ 
-    path('', views.home, name='home'),
-    path('upload', views.upload, name='upload'),
-    path('download', views.download, name='download'),
-]
